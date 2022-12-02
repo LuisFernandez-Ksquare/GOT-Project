049905d4fec8c7e6aba81a1e29214448991c5752
Brandon Hernandez <Brandon.Hernandez@itksquare.edu.mx>
2022-12-01 23:06:10 -0600

    done, just needs css

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector('.navbar').style.top = "0";
+    document.getElementById("navbar").style.top = "0";
   } else {
-    document.querySelector('.navbar').style.top = "-120px";
+    document.getElementById("navbar").style.top = "-50px";
   }
 }
